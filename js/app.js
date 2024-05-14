e4fcb87b8244c7a1b8bb5f8dcd972a44aa585d1c
Ali Haider <gorayaali80@gmail.com>
2024-05-14 22:58:32 +0500

    add pop up

--- js/app.js
+++ js/app.js
@@ -10,3 +10,37 @@ function collapseBtnsEvt() {
   })
 }
 collapseBtnsEvt()
+
+function addToCart() {
+  let heartIcons = document.querySelectorAll(".heart-circle")
+  let element = document.querySelector('[data-bs-target="#modal-1"]')
+
+  // Change the data-bs-target value
+
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 576) {
+      element.removeAttribute("data-bs-target")
+      console.log("removed")
+    } else {
+      element.setAttribute("data-bs-target", "#modal-1")
+      console.log("added")
+    }
+  })
+  console.log(element, window.innerWidth)
+
+  let color = "red"
+  heartIcons.forEach((heartIcon) => {
+    heartIcon.addEventListener("click", (e) => {
+      let icon = heartIcon.querySelector("i")
+      icon.classList.toggle("fa-solid")
+      if (color === "red") {
+        icon.style.color = color
+        color = "#7a7a7a"
+      } else {
+        icon.style.color = color
+        color = "red"
+      }
+    })
+  })
+}
+addToCart()
