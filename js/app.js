8483649cf87ea8070dde4b287aa53cc1c99993f6
Ali Haider <gorayaali80@gmail.com>
2024-05-14 18:27:47 +0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function collapseBtnsEvt() {
+  let collapseBtns = document.querySelectorAll("#collapse-toggle-btns button")
+
+  collapseBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      let icon = btn.querySelector("i")
+      icon.classList.toggle("fa-minus")
+      icon.classList.toggle("fa-plus")
+    })
+  })
+}
